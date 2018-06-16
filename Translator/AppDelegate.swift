5c65f6c51808e8916b3ec2735882a40af1b0fc5a
Alex Bass <alexpjbass@gmail.com>
2018-06-16 20:57:28 +0200

    Initial Commit

---
+++ Translator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Translator
+//
+//  Created by Alex Bass on 16/06/2018.
+//  Copyright © 2018 Alex Bass. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
